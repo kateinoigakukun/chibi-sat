5368f6e59f5f583f59f2b2bbedcd16cfc5d625ee
Yuta Saito <kateinoigakukun@gmail.com>
2021-09-26 15:51:28 +0900

    test: add compatibility check test runner

---
+++ Benchmarks/compat_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+CHIBI_SAT=${CHIBI_SAT:-$REPO_ROOT/.build/release/chibi-sat}
+PICOSAT=${PICOSAT:-$REPO_ROOT/Benchmarks/build/picosat/picosat}
+
+for i in $(seq 2 29); do
+  target_cnf=$REPO_ROOT/Benchmarks/small-cnf/small-cnf-${i}.cnf
+  $CHIBI_SAT $target_cnf > /dev/null 2>&1
+  actual_code=$?
+  $PICOSAT $target_cnf > /dev/null 2>&1
+  expected_code=$?
+
+  if [[ $actual_code != $expected_code ]]; then
+    echo "The result of $target_cnf is not compatible"
+    exit 1
+  fi
+
+  printf .
+done
