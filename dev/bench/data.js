b30641a8cb5ce659a1d49c42d44c9ff5fb0bb6e4
github-action-benchmark <github@users.noreply.github.com>
2021-09-26 06:00:37 +0000

    add ChibiSAT Benchmark (googlecpp) benchmark result for fc0351fd6a8ff0c5086294103b41b4bc59db4206

---
+++ dev/bench/data.js
@@ -0,0 +1,172 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1632636037307,
+  "repoUrl": "https://github.com/kateinoigakukun/chibi-sat",
+  "entries": {
+    "ChibiSAT Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kateinoigakukun@gmail.com",
+            "name": "Yuta Saito",
+            "username": "kateinoigakukun"
+          },
+          "committer": {
+            "email": "kateinoigakukun@gmail.com",
+            "name": "Yuta Saito",
+            "username": "kateinoigakukun"
+          },
+          "distinct": true,
+          "id": "fc0351fd6a8ff0c5086294103b41b4bc59db4206",
+          "message": "code: initial baseline implementation",
+          "timestamp": "2021-09-26T14:57:25+09:00",
+          "tree_id": "5790c65a522f79ade3c5623cb9e4700d629bc215",
+          "url": "https://github.com/kateinoigakukun/chibi-sat/commit/fc0351fd6a8ff0c5086294103b41b4bc59db4206"
+        },
+        "date": 1632636037290,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "small-cnf-2.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-3.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-4.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-5.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-6.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-7.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-8.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-9.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-10.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-11.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-12.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-13.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-14.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-15.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-16.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-17.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-18.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-19.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-20.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-21.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-22.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-23.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-24.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-25.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-26.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-27.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-28.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          },
+          {
+            "name": "small-cnf-29.cnf",
+            "unit": "undefined/iter",
+            "extra": "iterations: 2\ncpu: undefined undefined\nthreads: undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
